b136fe919a7ed62174e78a2f445434c7c444af53
masud040 <masudinfo962@gmail.com>
2024-03-25 15:25:03 +0600

    add a header for show home and category page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,24 +6,36 @@ const Navbar = () => {
       <div className="flex gap-5 items-center justify-between lg:w-8/12 text-[#1A1A1A]">
         <div className="flex items-center gap-2">
           <Image
-            src="./assets/svg/menu.svg"
+            src="/menu.svg"
             className="w-5 h-5 lg:hidden"
             alt=""
+            height={20}
+            width={20}
           />
           <a href="./index.html">
-            <Image src="./assets/lws-logo-black.svg" className="h-10" alt="" />
+            <Image
+              src="/lws-logo-black.svg"
+              className="h-10"
+              height={110}
+              width={110}
+              alt=""
+            />
           </a>
         </div>
       </div>
       <div className="flex items-center justify-end gap-2 lg:gap-5 lg:w-4/12">
         <Image
-          src="./assets/svg/avatar.svg"
+          src="/avatar.svg"
           className="hidden lg:block w-[18px] h-[18px]"
+          height={20}
+          width={20}
           alt="login acount avatar"
         />
         <Image
-          src="./assets/svg/shopping-Cart.svg"
+          src="/shopping-Cart.svg"
           className="block w-5 h-5"
+          height={20}
+          width={20}
           alt="shopping cart icon"
         />
       </div>
